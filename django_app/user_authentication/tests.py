9541e516e5b82242b39e59803f3a111e8600c022
Paulina Olejniczak <olejniczakpp@gmail.com>
2017-12-07 15:18:42 +0100

    correct errors in user_auth and add more tests

--- django_app/user_authentication/tests.py
+++ django_app/user_authentication/tests.py
@@ -43,4 +43,47 @@ class RegistrationTest(TestCase):
         self.assertFalse(form.is_valid())
 
 
+class ViewTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='user', email="user@fis.agh.edu.pl", password="12345678",
+                                             first_name="user", last_name="user")
 
+    def test_user_log_in(self):
+        self.assertTrue(self.client.login(username='user', password='12345678'))
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main_page/main_page.html')
+
+    def test_user_not_log_in(self):
+        response = self.client.get('/', follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/login.html')
+
+    def test_user_log_out(self):
+        self.client.login(username='user', password='12345678')
+        response = self.client.get('/logout/', follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_register(self):
+        user_count = User.objects.count()
+        response = self.client.post('/register/', {'username': 'user1', 'first_name': 'John', 'last_name': 'John',
+                                                   'email': 'john2@fis.agh.edu.pl', 'password': '12345678',
+                                                   'password_2': '12345678'}, follow=True)
+        self.assertTemplateUsed(response, 'user_authentication/register_done.html')
+        self.assertEqual(User.objects.count(), user_count + 1)
+
+    def test_wrong_register(self):
+        response = self.client.post('/register/', {'username': '', 'first_name': 'John', 'last_name': 'John',
+                                                   'email': 'john2@fis.agh.edu.pl', 'password': '12345',
+                                                   'password_2': '12345'}, follow=True)
+        self.assertTemplateUsed(response, 'user_authentication/register.html')
+
+        response = self.client.post('/register/', {'username': 'user', 'first_name': 'John', 'last_name': 'John',
+                                                   'email': 'john2@fis.agh.edu.pl', 'password': '12345678',
+                                                   'password_2': '12345678'}, follow=True)
+        self.assertTemplateUsed(response, 'user_authentication/register.html')
+
+        response = self.client.post('/register/', {'username': 'user11', 'first_name': 'John', 'last_name': 'John',
+                                                   'email': '', 'password': '12345678',
+                                                   'password_2': '12345678'}, follow=True)
+        self.assertTemplateUsed(response, 'user_authentication/register.html')
