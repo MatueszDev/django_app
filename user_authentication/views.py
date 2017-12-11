9fb9592644e4078f0e665a65e646f865aab31fea
Paulina Olejniczak <olejniczakpp@gmail.com>
2017-12-11 10:45:40 +0100

    add edit account

--- user_authentication/views.py
+++ user_authentication/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 def register(request):
@@ -11,7 +14,23 @@ def register(request):
             new_user = register_form.save(commit=False)
             new_user.set_password(register_form.cleaned_data['password'])
             new_user.save()
+            profile = Profile.objects.create(user=new_user)
             return render(request, 'user_authentication/register_done.html', {'new_user': new_user})
     else:
         register_form = UserRegistrationForm()
     return render(request, 'user_authentication/register.html', {'register_form': register_form})
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'The upgrade of the profile was successful.')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, 'user_authentication/edit.html', {'user_form': user_form,  'profile_form': profile_form})
