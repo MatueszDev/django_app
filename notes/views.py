bc7bd82a30a6378f0a59c6747bc83747378ce1fc
Jakub Ahaddad <j.ahaddad@protonmail.com>
2017-12-31 12:59:26 +0100

    notes/views cleanup, unifying file upload

--- notes/views.py
+++ notes/views.py
@@ -2,92 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from .models import NoteFileText, NoteFileImage, NoteFilePdf, Note
-from forms import NoteImageForm, NoteTextForm, NotePdfForm, NoteForm
+from .models import Note, NoteFileText, NoteFileImage, NoteFilePdf, NoteFileOther
+from forms import NoteForm, NoteImageForm, NoteTextForm, NotePdfForm, NoteOtherForm
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response
 from django.template.context_processors import csrf
 from django.db.models import Max
-
-def dev_fileview(request):
-    objects = NoteFileText.objects.all()
-    return render(request, 'dev_fileview/dev_fileview.html', {'objects': objects})
-
-def dev_imgview(request):
-    objects = NoteFileImage.objects.all()
-    return render(request, 'dev_fileview/dev_imgview.html', {'objects': objects})
-
-#note: displaying pdfs in a template requires javascript
-#you'll have to give me a few moments to figure that one out
-#also as of now it only displays the index 0 pdf
-def dev_pdfview(request):
-    objects = NoteFilePdf.objects.all()
-    return render(request, 'dev_fileview/dev_pdfview.html', {'objects': objects})
-#   #   the following code is a view for displaying a specific pdf
-#    with open(objects[0].content.path,'r') as pdf:
-#        response = HttpResponse(pdf.read(), content_type='application/pdf')
-#        response['Content-Disposition'] = 'inline;filename='+objects[0].content.name
-#        return response
-
-def add_file(request):
-    '''test: sorting uploaded files by their type'''
-#    if request.POST:
-#        for filename,file_ in request.FILES.iteritems():
-#            name = request.FILES[filename].name
-#
-    pass
-
-def add_image(request):
-    if request.POST:
-        form = NoteImageForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-
-            return HttpResponseRedirect('/notes')
-    else:
-        form = NoteImageForm(request.POST)
-
-    args = {}
-    args.update(csrf(request))
-
-    args['form'] = form
-
-    return render_to_response('add_image.html', args)
-    
-def add_text(request):
-    if request.POST:
-        form = NoteTextForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-
-            return HttpResponseRedirect('/notes')
-    else:
-        form = NoteTextForm(request.POST)
-
-    args = {}
-    args.update(csrf(request))
-
-    args['form'] = form
-
-    return render_to_response('add_text.html', args)
-    
-def add_pdf(request):
-    '''Temporary for testing until add_file is finished'''
-    if request.POST:
-        form = NotePdfForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-
-            return HttpResponseRedirect('/notes')
-    else:
-        form = NotePdfForm(request.POST)
-
-    args = {}
-    args.update(csrf(request))
-
-    args['form'] = form
-
-    return render_to_response('add_pdf.html', args)
 
 def add_note(request):
     if request.POST:
@@ -106,6 +26,32 @@ def add_note(request):
 
     return render_to_response('add_note.html', args)
 
+def add_file(request):
+    if request.POST:
+        form = NoteOtherForm(request.POST, request.FILES)
+        for filename,file_ in request.FILES.iteritems():
+            print file_.content_type
+            if file_.content_type.startswith("text/"):
+                form = NoteTextForm(request.POST, request.FILES)
+            elif file_.content_type.startswith("image/"):
+                form = NoteImageForm(request.POST, request.FILES)
+            elif file_.content_type.startswith("application/pdf"):
+                form = NotePdfForm(request.POST, request.FILES)
+            
+        if form.is_valid():
+            form.save()
+
+            return HttpResponseRedirect('/notes')
+    else:
+        form = NoteOtherForm(request.POST)
+
+    args = {}
+    args.update(csrf(request))
+
+    args['form'] = form
+
+    return render_to_response('add_file.html', args)
+
 def choose_class(request):
     objects = Note.objects.all()
     
@@ -118,11 +64,13 @@ def select_lecture(request,subject,lecture_number):
     text = NoteFileText.objects.all()
     image = NoteFileImage.objects.all()
     pdf = NoteFilePdf.objects.all()
+    other = NoteFileOther.objects.all()
     
     objects = note.filter(subject=subject, lecture_number=lecture_number)
     text_objs = text.filter(subject=subject, lecture_number=lecture_number)
     img_objs = image.filter(subject=subject, lecture_number=lecture_number)
     pdf_objs = pdf.filter(subject=subject, lecture_number=lecture_number)
+    otr_objs = other.filter(subject=subject, lecture_number=lecture_number)
     
     lecture_title = objects[0].lecture_title
     
@@ -132,4 +80,5 @@ def select_lecture(request,subject,lecture_number):
                                             'lecture_title': lecture_title,
                                             'text_objs' : text_objs,
                                             'img_objs' : img_objs,
-                                            'pdf_objs' : pdf_objs})
+                                            'pdf_objs' : pdf_objs,
+                                            'otr_objs' : otr_objs})
