7e938dd6cd79d5e1ac12de2eefde33976436a3bc
Paulina Olejniczak <olejniczakpp@gmail.com>
2017-12-31 13:39:33 +0100

    changes

--- user_authentication/models.py
+++ user_authentication/models.py
@@ -10,6 +10,3 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
     date_of_birth = models.DateField(blank=True, null=True)
     subjects = models.CharField(blank=True, null=True, max_length=100)
-
-    def __str__(self):
-        return 'User account {}.'.format(self.user.username)
