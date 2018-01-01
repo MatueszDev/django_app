5c3fd51ec778997aca25f08ddb83290c3acc47da
Krystian Kapinos <Krystian.Kapinos@fis.agh.edu.pl>
2018-01-01 14:00:44 +0100

    Changed from sqlite to postgresql

--- django_app/settings.py
+++ django_app/settings.py
@@ -96,11 +96,8 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'TEST': {
-            'NAME': 'test',
-        },
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.path.join(BASE_DIR, 'db'),
     }
 }
 
