7fb27294a40a5be441922a98930ad1a0a70e28f0
Krystian Kapinos <Krystian.Kapinos@fis.agh.edu.pl>
2018-01-01 12:27:11 +0100

    db test

--- django_app/settings.py
+++ django_app/settings.py
@@ -94,12 +94,16 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+import dj_database_url
+
+DATABASES['default'] =  dj_database_url.config()
+
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+# }
 
 
 # Password validation
