7a94384d38bf02b43eaec5da2b8fd761c5fd34b6
MatueszDev <m.seweryn96@gmail.com>
2018-01-03 01:12:51 +0100

    more poll staff

--- poll/urls.py
+++ poll/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<object_id>\d+)/$', views.poll, name='poll'),
     url(r'^addPoll/$', views.create_poll, name='create_poll'),
-    url(r'^(?P<object_id>\d+)/vote/(?P<option>\w+)/$', views.vote, name='vote')
+    url(r'^(?P<object_id>\d+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<object_id>\d+)/delete/(?P<object_id_2>\d+)$', views.delete_respond, name='delete_respond'),
 ]
--- poll/views.py
+++ poll/views.py
@@ -25,7 +25,7 @@ def poll(request, object_id):
 
             number = Respond.number_of_answers(poll[0])
 
-            if number > 11:
+            if number > 15:
                 text_info = 'Numbers of answers can not be grater than 15, if you want add another answers delete previous one.'
 
                 answers = Respond.objects.filter(poll=poll)
@@ -42,12 +42,12 @@ def poll(request, object_id):
 
         return HttpResponseRedirect(reverse('poll', kwargs={ 'object_id': object_id}) )
     else:
-
+        info_2 = request.GET.get('info-2', None)
         question = Poll.objects.filter(id=object_id)
         answers = Respond.objects.filter(poll=question)
         form = AnsForm()
 
-        extra_content = {'question':question, 'answers':answers, 'form':form, 'id':object_id}
+        extra_content = {'question':question, 'answers':answers, 'form':form, 'id':object_id, 'info_2':info_2}
 
 
         return render(request, "poll/poll.html", extra_content)
@@ -79,7 +79,9 @@ def create_poll(request):
 
     return render(request, "poll/addPoll.html", {'form': form})
 
-def vote(request,object_id, option=None):
+def vote(request,object_id):
+    option = request.GET.get('option')
+
     vote = Vote()
     vote.poll = Poll.objects.filter(id=object_id)[0]
     vote.user = request.user
@@ -87,3 +89,26 @@ def vote(request,object_id, option=None):
     vote.save()
 
     return HttpResponseRedirect('/poll/%s/' % object_id)
+
+def delete_respond(request, object_id, object_id_2):
+    poll = Poll.objects.filter(id=object_id)
+    answer = Respond.objects.filter(id=object_id_2)
+    votes = Vote.objects.filter(choice=answer[0])
+
+    if(Respond.number_of_answers(poll[0]) <= 2):
+        text_info = "There must stay at least two answers."
+
+        return HttpResponseRedirect("/poll/%s/?info-2=%s" % (object_id, text_info))
+
+    number_of_ans = Vote.count_option_votes(answer[0])
+
+    for i in range(number_of_ans):
+        votes[i].delete()
+
+    answer[0].delete()
+
+    return HttpResponseRedirect('/poll/%s/' % object_id)
+
+def delete_poll(request):
+    pass
+
