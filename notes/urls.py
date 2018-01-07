84601effcecaa1ce04bb35070f67e2d2696f1dfa
Jakub Ahaddad <j.ahaddad@protonmail.com>
2018-01-07 18:30:12 +0100

    connecting notes to groups, part 2

--- notes/urls.py
+++ notes/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     url(r'^(?P<classes>[A-Za-z0-9]+)/(?P<lecture_number>[0-9]+)/$', views.select_lecture, name='select_lecture'),
     url(r'^(?P<classes>[A-Za-z0-9]+)/(?P<lecture_number>[0-9]+)/add_note/$', views.add_note, name='add_note'),
     url(r'^(?P<classes>[A-Za-z0-9]+)/(?P<lecture_number>[0-9]+)/add_file/$', views.add_file, name='add_file'),
-#    url(r'^add_note/$', views.add_note, name='add_note'),
-#    url(r'^add_file/$', views.add_file, name='add_file'),
+    url(r'^add_lecture/$', views.add_lecture, name='add_lecture'),
 ]
