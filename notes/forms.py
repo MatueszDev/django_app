7f1efd24668b0bd9318339d99321826dd5f55587
Jakub Ahaddad <j.ahaddad@protonmail.com>
2018-01-09 14:20:55 +0100

    minor corrections

--- notes/forms.py
+++ notes/forms.py
@@ -11,25 +11,25 @@ class NoteForm(forms.ModelForm):
 class NoteTextForm(forms.ModelForm):
     class Meta:
         model = NoteFileText
-        fields = ('name', 'author', 'lecture', 'content')
+        fields = ('title', 'author', 'lecture', 'content')
         exclude = ['author','lecture']
 
 class NoteImageForm(forms.ModelForm):
     class Meta:
         model = NoteFileImage
-        fields = ('name', 'author', 'lecture', 'content')
+        fields = ('title', 'author', 'lecture', 'content')
         exclude = ['author','lecture']
 
 class NotePdfForm(forms.ModelForm):
     class Meta:
         model = NoteFilePdf
-        fields = ('name', 'author', 'lecture', 'content')
+        fields = ('title', 'author', 'lecture', 'content')
         exclude = ['author','lecture']
 
 class NoteOtherForm(forms.ModelForm):
     class Meta:
         model = NoteFileOther
-        fields = ('name', 'author', 'lecture', 'content')
+        fields = ('title', 'author', 'lecture', 'content')
         exclude = ['author','lecture']
 
 class LectureForm(forms.ModelForm):
